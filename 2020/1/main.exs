e08566de1401ebc2aedee8c0d1345d52cdb1512c
olivier <olivier@localhost.localdomain>
2020-12-03 19:42:19 +0100

    Supprime le cas où a+a=2020...

--- 2020/1/main.exs
+++ 2020/1/main.exs
@@ -1,9 +1,10 @@
-{ok, text} = File.read "input"
+{:ok, text} = File.read "input"
 
 intList = text |> (String.split "\n") |> (Enum.map &(String.to_integer &1))
 
 # First part
-(for a <- intList, b <- intList, a + b == 2020, do: a * b) |> hd |> IO.puts
+(for a <- intList, b <- intList, a + b == 2020, a != b, do: a * b) |> hd |> IO.puts
 
 # Second part
-(for a <- intList, b <- intList, c <- intList, a + b + c == 2020, do: a * b * c) |> hd |> IO.puts
+(for a <- intList, b <- intList, c <- intList, a + b + c == 2020, a != b, b != c, do: a * b * c) |> hd |> IO.puts
+
